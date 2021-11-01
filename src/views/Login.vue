8289c89f2c3badc1ef64a17680e309d73df1a124
Felipe Sanchez <vegafelipe015@gmail.com>
2021-10-31 22:02:18 -0500

    md5 hash

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -10,7 +10,9 @@
                   <v-row class="fill-height">
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 blue--text" >Iniciar sesión</h1>
+                        <h1 class="text-center display-2 blue--text">
+                          Iniciar sesión
+                        </h1>
                         <v-form>
                           <v-text-field
                             label="Email"
@@ -33,15 +35,25 @@
                         </v-form>
                       </v-card-text>
                       <div class="text-center mt-3 mb-3">
-                        <v-btn rounded color="blue lighten-2" dark @click="sigIn()">Iniciar sesión</v-btn>
+                        <v-btn
+                          rounded
+                          color="blue lighten-2"
+                          dark
+                          @click="sigIn()"
+                          >Iniciar sesión</v-btn
+                        >
                       </div>
                     </v-col>
                     <v-col cols="12" md="4" sm="6" class="blue lighten-2">
-                      <v-card-text class="white--text mt-8"> 
-                        <h1 class="text-center display-1">Si no tienes una cuenta, registrate!</h1>
+                      <v-card-text class="white--text mt-8">
+                        <h1 class="text-center display-1">
+                          Si no tienes una cuenta, registrate!
+                        </h1>
                       </v-card-text>
                       <div class="text-center">
-                        <v-btn rounded outlined color="white" @click="step+=1">Registrarse</v-btn>
+                        <v-btn rounded outlined color="white" @click="step += 1"
+                          >Registrarse</v-btn
+                        >
                       </div>
                     </v-col>
                   </v-row>
@@ -51,21 +63,24 @@
                     <v-col cols="12" md="4" class="blue lighten-2">
                       <v-card-text class="white--text mt-12">
                         <h1 class="text-center display-1">Iniciar sesión</h1>
-                        <h4
-                          class="text-center"
-                        >Si ya tienes una cuenta, inicia sesión con tu usuario y contraseña</h4>
+                        <h4 class="text-center">
+                          Si ya tienes una cuenta, inicia sesión con tu usuario
+                          y contraseña
+                        </h4>
                       </v-card-text>
                       <div class="text-center">
-                        <v-btn rounded outlined dark @click="step--">Ir al inicio de sesión</v-btn>
+                        <v-btn rounded outlined dark @click="step--"
+                          >Ir al inicio de sesión</v-btn
+                        >
                       </div>
                     </v-col>
 
                     <v-col cols="12" md="8">
                       <v-card-text class="mt-12">
-                        <h1 class="text-center display-2 blue--text ">Crear cuenta</h1>
-                        <div class="text-center mt-4">
-                          
-                        </div>
+                        <h1 class="text-center display-2 blue--text">
+                          Crear cuenta
+                        </h1>
+                        <div class="text-center mt-4"></div>
                         <v-form>
                           <v-text-field
                             label="Nombre"
@@ -104,7 +119,13 @@
                         </v-form>
                       </v-card-text>
                       <div class="text-center mt-n5">
-                        <v-btn rounded color="blue lighten-2 mb-4" dark @click="singUp()">Registrarse</v-btn>
+                        <v-btn
+                          rounded
+                          color="blue lighten-2 mb-4"
+                          dark
+                          @click="singUp()"
+                          >Registrarse</v-btn
+                        >
                       </div>
                     </v-col>
                   </v-row>
@@ -119,69 +140,87 @@
 </template>
 
 <script>
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"
-import { setDoc , doc} from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { setDoc, doc } from "firebase/firestore";
+//import MD5 from "crypto-js/md5"; comentado para evitar errores
 
+export default {
+  name: "Login",
+  data: () => ({
+    expandOnHover: false,
+    step: 1,
+    userToRegister: {
+      name: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    userToLogIn: {
+      email: "",
+      password: "",
+    },
+  }),
+  methods: {
+    sigIn() {
+      const auth = getAuth();
 
-  export default {
-    name: 'Login',
-    data: () => ({
-      expandOnHover: false,
-      step:1,
-      userToRegister:{
-        name: '',
-        lastName: '',
-        email: '',
-        password:'',
-      },
-      userToLogIn:{
-        email: '',
-        password:'',
-      }
-    }),
-    methods:{
-      sigIn(){
-        const auth = getAuth();
-        signInWithEmailAndPassword(auth, this.userToLogIn.email, this.userToLogIn.password)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            console.log(user);
-            this.$router.push('/users');
-            // ...
+      //const hash_password = MD5(this.userToLogIn.password).toString;
+      // no la uso aun porque las cuentas actuales tienen las contraseñas sin hash
+
+      signInWithEmailAndPassword(
+        auth,
+        this.userToLogIn.email,
+        this.userToLogIn.password
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          this.$router.push("/users");
+          // ...
+        })
+        .catch((error) => {
+          this.error = error.message;
+        });
+    },
+    singUp() {
+      const auth = getAuth();
+
+      //const hash_password = MD5(this.userToLogIn.password).toString;
+
+      createUserWithEmailAndPassword(
+        auth,
+        this.userToRegister.email,
+        this.userToRegister.password
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          const db = getFirestore();
+
+          setDoc(doc(db, "users", user.uid), {
+            id: user.uid,
+            name: this.userToRegister.name,
+            lastName: this.userToRegister.lastName,
+            email: this.userToRegister.email,
           })
-          .catch((error) => {
-            this.error = error.message;
-          });
-      },
-      singUp(){
-        const auth = getAuth();
-        createUserWithEmailAndPassword(auth, this.userToRegister.email, this.userToRegister.password)
-          .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            console.log(user);
-            const db = getFirestore();
-          
-              setDoc(doc(db, "users", user.uid), {
-                id : user.uid,
-                name : this.userToRegister.name,
-                lastName : this.userToRegister.lastName,
-                email : this.userToRegister.email
-              }).then(()=>{
-                this.$router.push('/users');
-              }).catch((error)=>{
-                  this.error = error.message;
-              });
-              
-           
-          })
-          .catch((error) => {
-            this.error = error.message;
-          });
-        
-      },
-    }
-  }
+            .then(() => {
+              this.$router.push("/users");
+            })
+            .catch((error) => {
+              this.error = error.message;
+            });
+        })
+        .catch((error) => {
+          this.error = error.message;
+        });
+    },
+  },
+};
 </script>
